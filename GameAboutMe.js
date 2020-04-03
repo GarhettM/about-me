9ec8977aef70047c6cd812855406364e45196715
GarhettM <32fossil@gmail.com>
2020-04-02 20:06:40 -0700

    Added function for q1

--- GameAboutMe.js
+++ GameAboutMe.js
@@ -1,72 +1,75 @@
-// 'use strict';
+'use strict';
 
-// // User is greated by the web page. The following are my global, top-level, variables for this set of labs.
+// User is greated by the web page. The following are my global, top-level, variables for this set of labs.
 
-// var userName = prompt('Welcome to the world\'s most rudimentary guessing game! So, um, what\'s your name?');
-// var userInput;
-// var userScore = 0;
+var userName = prompt('Welcome to the world\'s most rudimentary guessing game! So, um, what\'s your name?');
+var userInput;
+var userScore = 0;
 
-// // Page knows the user now and begins the guessing game. NOTE TO SELF --- ADD User Score Increment later after each correct answer!
+// Page knows the user now and begins the guessing game. NOTE TO SELF --- ADD User Score Increment later after each correct answer!
 
-// alert('Hi ' + userName + '! Let\'s play a game about me!');
-// console.log(userName);
+alert('Hi ' + userName + '! Let\'s play a game about me!');
+console.log(userName);
 
-// // START OF GAME!
+// START OF GAME!
 
-// alert('Well, ' + userName + ', I hope you read the autobiography. Here we go!');
+alert('Well, ' + userName + ', I hope you read the autobiography. Here we go!');
 
-// // Question 1 = Was I born in the United States of America? Answer = NO
+// Question 1 = Was I born in the United States of America? Answer = NO
 
-// var answerToFirstQ = prompt('Was I born in the United States?').toUpperCase();
+function q1() {
 
-// if (answerToFirstQ === 'N' || answerToFirstQ === 'NO') {
-//   alert('Correct! Looks like you actually read the bio!');
-//   userScore++
-// }else if (answerToFirstQ === 'Y' || answerToFirstQ === 'YES') {
-//   alert('Um, ya dun goofed!');
-// }else{alert('Please type accurately! Answer YES or NO!')};
+  var answerToFirstQ = prompt('Was I born in the United States?').toUpperCase();
 
-// // Question 2 = Am I fluent in two languages? Answer = YES
+  if (answerToFirstQ === 'N' || answerToFirstQ === 'NO') {
+    alert('Correct! Looks like you actually read the bio!');
+    userScore++
+  }else if (answerToFirstQ === 'Y' || answerToFirstQ === 'YES') {
+    alert('Um, ya dun goofed!');
+  }else{alert('Please type accurately! Answer YES or NO!')};
 
-// var answerToSecondQ = prompt('Am I fluent in two languages?').toUpperCase();
+}
+// Question 2 = Am I fluent in two languages? Answer = YES
 
-// if (answerToSecondQ === 'Y' || answerToSecondQ === 'YES') {
-//   alert('Correct! Let\'s keep going!');
-// }else if (answerToSecondQ === 'N' || answerToSecondQ === 'NO') {
-//   alert('Um, ya dun goofed!');
-// }else{alert('Please type accurately! Answer YES or NO!')};
+var answerToSecondQ = prompt('Am I fluent in two languages?').toUpperCase();
 
-// // Question 3 = Am I currently an effective front end web developer? Answer = NO
+if (answerToSecondQ === 'Y' || answerToSecondQ === 'YES') {
+  alert('Correct! Let\'s keep going!');
+}else if (answerToSecondQ === 'N' || answerToSecondQ === 'NO') {
+  alert('Um, ya dun goofed!');
+}else{alert('Please type accurately! Answer YES or NO!')};
 
-// var answerToThirdQ = prompt('Am I currently an effective front end web developer?').toUpperCase();
+// Question 3 = Am I currently an effective front end web developer? Answer = NO
 
-// if (answerToThirdQ === 'N' || answerToThirdQ === 'NO') {
-//   alert('Correct! You\'re a savant!');
-// }else if (answerToThirdQ === 'Y' || answerToThirdQ === 'YES') {
-//   alert('Um, ya dun goofed!');
-// }else{alert('Please type accurately! Answer YES or NO!')};
+var answerToThirdQ = prompt('Am I currently an effective front end web developer?').toUpperCase();
 
-// // Question 4 = Will I become an effective front end web developer? Answer = YES! one day...
+if (answerToThirdQ === 'N' || answerToThirdQ === 'NO') {
+  alert('Correct! You\'re a savant!');
+}else if (answerToThirdQ === 'Y' || answerToThirdQ === 'YES') {
+  alert('Um, ya dun goofed!');
+}else{alert('Please type accurately! Answer YES or NO!')};
 
-// var answerToFourthQ = prompt('Will I eventually become an effective front end web developer?').toUpperCase();
+// Question 4 = Will I become an effective front end web developer? Answer = YES! one day...
 
-// if (answerToFourthQ === 'Y' || answerToFourthQ === 'YES') {
-//   alert('Correct! Let\'s keep going!');
-// }else if (answerToFourthQ === 'N' || answerToFourthQ === 'NO') {
-//   alert('Um, ya dun goofed!');
-// }else{alert('Please type accurately! Answer YES or NO!')};
+var answerToFourthQ = prompt('Will I eventually become an effective front end web developer?').toUpperCase();
 
-// // Question 5 = Do I have a professional background in Business Administration and Sales? Answer = YES
+if (answerToFourthQ === 'Y' || answerToFourthQ === 'YES') {
+  alert('Correct! Let\'s keep going!');
+}else if (answerToFourthQ === 'N' || answerToFourthQ === 'NO') {
+  alert('Um, ya dun goofed!');
+}else{alert('Please type accurately! Answer YES or NO!')};
 
-// var answerToFifthQ = prompt('Do I have a professional background in Business Administration and Sales?').toUpperCase();
+// Question 5 = Do I have a professional background in Business Administration and Sales? Answer = YES
 
-// if (answerToFifthQ === 'Y' || answerToFifthQ === 'YES') {
-//   alert('Correct! Let\'s keep going!');
-// }else if (answerToFifthQ === 'N' || answerToFifthQ === 'NO') {
-//   alert('Um, ya dun goofed!');
-// }
+var answerToFifthQ = prompt('Do I have a professional background in Business Administration and Sales?').toUpperCase();
 
-// alert('Well that wasn\'t terrible, let\'s try something harder!')
+if (answerToFifthQ === 'Y' || answerToFifthQ === 'YES') {
+  alert('Correct! Let\'s keep going!');
+}else if (answerToFifthQ === 'N' || answerToFifthQ === 'NO') {
+  alert('Um, ya dun goofed!');
+}
+
+alert('Well that wasn\'t terrible, let\'s try something harder!')
 
 function questionSix()  {
   var counter = 0;
@@ -93,13 +96,13 @@ function questionSix()  {
     
   }
 
-  alert('Well that wasn\'t terrible, let\'s try something harder!')
+  alert('Next hard question!')
 
   } 
 
 
 
-questionSix();
+
 
 
 function questionSeven()  {
@@ -125,4 +128,6 @@ function questionSeven()  {
 
 }
 
+q1();
+questionSix();
 questionSeven();
