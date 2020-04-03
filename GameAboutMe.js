579e55eea4b22194c16827a94a3d32ce3da1bfa1
Davidoffili <palagashvilid@gmail.com>
2020-04-02 17:11:36 -0700

    experimented with while loop inside a function

--- GameAboutMe.js
+++ GameAboutMe.js
@@ -2,87 +2,109 @@
 
 // User is greated by the web page. The following are my global, top-level, variables for this set of labs.
 
-var userName = prompt('Welcome to the world\'s most rudimentary guessing game! So, um, what\'s your name?');
-var userInput;
-var userScore = 0;
+// var userName = prompt('Welcome to the world\'s most rudimentary guessing game! So, um, what\'s your name?');
+// var userInput;
+// var userScore = 0;
 
 // Page knows the user now and begins the guessing game. NOTE TO SELF --- ADD User Score Increment later after each correct answer!
 
-alert('Hi ' + userName + '! Let\'s play a game about me!');
-console.log(userName);
+// alert('Hi ' + userName + '! Let\'s play a game about me!');
+// console.log(userName);
 
-// START OF GAME!
+// // START OF GAME!
 
-alert('Well, ' + userName + ', I hope you read the autobiography. Here we go!');
+// alert('Well, ' + userName + ', I hope you read the autobiography. Here we go!');
 
-// Question 1 = Was I born in the United States of America? Answer = NO
+// // Question 1 = Was I born in the United States of America? Answer = NO
 
-var answerToFirstQ = prompt('Was I born in the United States?').toUpperCase();
+// var answerToFirstQ = prompt('Was I born in the United States?').toUpperCase();
 
-if (answerToFirstQ === 'N' || answerToFirstQ === 'NO') {
-  alert('Correct! Looks like you actually read the bio!');
-  userScore++
-}else if (answerToFirstQ === 'Y' || answerToFirstQ === 'YES') {
-  alert('Um, ya dun goofed!');
-}else{alert('Please type accurately! Answer YES or NO!')};
+// if (answerToFirstQ === 'N' || answerToFirstQ === 'NO') {
+//   alert('Correct! Looks like you actually read the bio!');
+//   userScore++
+// }else if (answerToFirstQ === 'Y' || answerToFirstQ === 'YES') {
+//   alert('Um, ya dun goofed!');
+// }else{alert('Please type accurately! Answer YES or NO!')};
 
-// Question 2 = Am I fluent in two languages? Answer = YES
+// // Question 2 = Am I fluent in two languages? Answer = YES
 
-var answerToSecondQ = prompt('Am I fluent in two languages?').toUpperCase();
+// var answerToSecondQ = prompt('Am I fluent in two languages?').toUpperCase();
 
-if (answerToSecondQ === 'Y' || answerToSecondQ === 'YES') {
-  alert('Correct! Let\'s keep going!');
-}else if (answerToSecondQ === 'N' || answerToSecondQ === 'NO') {
-  alert('Um, ya dun goofed!');
-}else{alert('Please type accurately! Answer YES or NO!')};
+// if (answerToSecondQ === 'Y' || answerToSecondQ === 'YES') {
+//   alert('Correct! Let\'s keep going!');
+//   userScore++
+// }else if (answerToSecondQ === 'N' || answerToSecondQ === 'NO') {
+//   alert('Um, ya dun goofed!');
+// }else{alert('Please type accurately! Answer YES or NO!')};
 
-// Question 3 = Am I currently an effective front end web developer? Answer = NO
+// // Question 3 = Am I currently an effective front end web developer? Answer = NO
 
-var answerToThirdQ = prompt('Am I currently an effective front end web developer?').toUpperCase();
+// var answerToThirdQ = prompt('Am I currently an effective front end web developer?').toUpperCase();
 
-if (answerToThirdQ === 'N' || answerToThirdQ === 'NO') {
-  alert('Correct! You\'re a savant!');
-}else if (answerToThirdQ === 'Y' || answerToThirdQ === 'YES') {
-  alert('Um, ya dun goofed!');
-}else{alert('Please type accurately! Answer YES or NO!')};
+// if (answerToThirdQ === 'N' || answerToThirdQ === 'NO') {
+//   alert('Correct! You\'re a savant!');
+//   userScore++
+// }else if (answerToThirdQ === 'Y' || answerToThirdQ === 'YES') {
+//   alert('Um, ya dun goofed!');
+// }else{alert('Please type accurately! Answer YES or NO!')};
 
-// Question 4 = Will I become an effective front end web developer? Answer = YES! one day...
+// // Question 4 = Will I become an effective front end web developer? Answer = YES! one day...
 
-var answerToFourthQ = prompt('Will I eventually become an effective front end web developer?').toUpperCase();
+// var answerToFourthQ = prompt('Will I eventually become an effective front end web developer?').toUpperCase();
 
-if (answerToFourthQ === 'Y' || answerToFourthQ === 'YES') {
-  alert('Correct! Let\'s keep going!');
-}else if (answerToFourthQ === 'N' || answerToFourthQ === 'NO') {
-  alert('Um, ya dun goofed!');
-}else{alert('Please type accurately! Answer YES or NO!')};
+// if (answerToFourthQ === 'Y' || answerToFourthQ === 'YES') {
+//   alert('Correct! Let\'s keep going!');
+//   userScore++
+// }else if (answerToFourthQ === 'N' || answerToFourthQ === 'NO') {
+//   alert('Um, ya dun goofed!');
+// }else{alert('Please type accurately! Answer YES or NO!')};
 
-// Question 5 = Do I have a professional background in Business Administration and Sales? Answer = YES
+// // Question 5 = Do I have a professional background in Business Administration and Sales? Answer = YES
 
-var answerToFifthQ = prompt('Do I have a professional background in Business Administration and Sales?').toUpperCase();
+// var answerToFifthQ = prompt('Do I have a professional background in Business Administration and Sales?').toUpperCase();
 
-if (answerToFifthQ === 'Y' || answerToFifthQ === 'YES') {
-  alert('Correct! Let\'s keep going!');
-}else if (answerToFifthQ === 'N' || answerToFifthQ === 'NO') {
-  alert('Um, ya dun goofed!');
+// if (answerToFifthQ === 'Y' || answerToFifthQ === 'YES') {
+//   alert('Correct! Let\'s keep going!');
+//   userScore++
+// }else if (answerToFifthQ === 'N' || answerToFifthQ === 'NO') {
+//   alert('Um, ya dun goofed!');
+// }
+
+// alert('Well that wasn\'t terrible, let\'s try something harder!')
+
+
+// We have a function called cities
+// You only have 3 attempts
+// The Question is 'How many cities have I lived in during my childhold?'
+// The answer to is the quanitity 5.
+// Counter
+
+
+// function askQuestion(); {
+//   var answer = 5;
+//   var attempts = 0;
+//   var question = prompt('How many cities?');
+//   var userInput;
+//   while (question !== answer && attempts < 3) {
+//   }
+// }
+
+function cities() {
+  var attemptsMade = 0;
+  var sixthQ = parseInt(prompt('How many cities have I lived in during my childhold?'));
+  var alertSix;
+  while(attemptsMade < 2 && sixthQ !== 5){
+    alertSix = prompt('Ya dun goofed Try again!');
+    attemptsMade++;
+  }
+  if (attemptsMade === 2){
+    confirm('You\'re a fool, please proceed to the next question or face an infinite loop!');
+  } else {
+    confirm('Congrats, you cared enough');
+  }
+  
 }
-
-alert('Well that wasn\'t terrible, let\'s try something harder!')
-
-var attemptsMade = 0;
-
-while(attemptsMades < 4){
-  var answerToSixthQ = parseInt(prompt('How many cities have I lived in during my childhold?'));
-  if(answerToSixthQ === 5){
-    alert('Wow! You actually paid close attention, you care! Congratulations, most people don\'t care!')
-    userScore++;
-    break;
-  } else if (answerToSixthQ > 5){
-    alert('Nah, TOO HIGH!')
-  } else if (answerToSixthQ < 5){
-    alert('Nah, TOO LOW!')
-  } else {alert('Um... really? Try a number!')}
-  attemptsMade++
-}
+cities();
 
 // alert('I\'ve lived in 5 cities during my childhood... come on, keep up!')
 
